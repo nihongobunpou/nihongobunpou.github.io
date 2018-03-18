06a30ecdb0116e7e7e598e768779bed05f395fb8
Nicholas Eckardt <neckardt@umich.edu>
2018-03-18 00:18:32 -0400

    Wrote most of the javascript, somewhat tested on the fly.
    Importing jquery and jquery ui.
    Conjugation module pulled from another github repo, appears to be working.
    Vocab grabbed from f17 1.1 and 3.1, in vocab.js
    Main function written. We now have:
     - check boxes working
     - coniguration saving to cookies
    Next time I will work on actually getting the logic running.

---
+++ conjugate.js
@@ -0,0 +1,172 @@
+
+
+var state = {}
+
+//TODO maybe history cause it'll look nice :)
+//by that I mean maybe like some basic analytics, how many thing we got
+//right, how many wrong, whatever
+state['history'] = null
+
+state['current'] = {query: null, solution: null}
+
+state['config'] = {
+	conjType: 
+		{ 	"plain-present-affirmative": true, 
+	   		"plain-present-negative": true,
+			"plain-past-affirmative": true, 
+			"plain-past-negative": true
+		}, 
+	vocabList:
+		{	
+			f17: 
+			{
+				"1.1": true, 
+				"3.1": true
+			}
+		}
+}
+
+function bake_cookie(name, value) {
+	var cookie = [name, '=', JSON.stringify(value),
+		'; '].join('');
+	document.cookie = cookie;
+}
+function read_cookie(name) {
+	var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+	result && (result = JSON.parse(result[1]));
+	return result;
+}
+
+function generateQuery()
+{
+	// TODO get all of the conjugation types in a list, then choose one
+
+	// TODO get all of the vocab words in a list, then choose one
+	for(var set in vobab)
+	{
+		for(var word in vocab['key'])
+		{
+
+		}
+	}
+
+
+	// TODO now generate the correct word based on the chosen words, and
+	// save it in the state, save the cookie, and update the conjQuery query
+	
+}
+
+//Get the state of each button, and use it to update our current state
+function updateStateFromBoxes()
+{
+	console.log(state)
+	// console.log($('#plain-present-affirmative:checked').val());
+	
+	// Grammar state
+	state['config']['conjType']['plain-present-affirmative'] = 
+		$('#plain-present-affirmative').is(":checked");
+	state['config']['conjType']['plain-present-negative'] = 
+		$('#plain-present-negative').is(":checked");
+	state['config']['conjType']['plain-past-affirmative'] = 
+		$('#plain-past-affirmative').is(":checked");
+	state['config']['conjType']['plain-past-negative'] = 
+		$('#plain-past-negative').is(":checked");
+
+	// Vocabulary Set State
+	// Vocab fall 2017
+	state['config']['vocabList']['f17']['1.1'] = 
+		$('#f17m1-1').is(":checked");
+	state['config']['vocabList']['f17']['3.1'] = 
+		$('#f17m3-1').is(":checked");
+
+}
+
+// Set the state of all of the buttons to what was in the cookies
+function setBoxState()
+{
+	//console.log(state)
+	//console.log(state['config']['conjType']['plain-present-affirmative']);
+	//console.log(state['config']['conjType']['plain-present-affirmative'] == 
+	//		"on" ? true : false);
+	// Grammar state
+	$('#plain-present-affirmative').prop('checked', 
+		state['config']['conjType']['plain-present-affirmative']);
+	$('#plain-present-negative').prop('checked', 
+		state['config']['conjType']['plain-present-negative']);
+	$('#plain-past-affirmative').prop('checked', 
+		state['config']['conjType']['plain-past-affirmative']);
+	$('#plain-past-negative').prop('checked', 
+		state['config']['conjType']['plain-past-negative'] );
+
+	// Vocabulary Set State
+	// Vocab fall 2017
+	$('#f17m1-1').prop('checked', 
+			state['config']['vocabList']['f17']['1.1']);
+	$('#f17m3-1').prop('checked', 
+			state['config']['vocabList']['f17']['3.1']);
+
+}
+
+function mainFunc()
+{
+	//All file loaded
+	
+	//TODO
+	//Load the settings from the cookie if there is one, otherwise keep
+	
+
+	if(read_cookie("conjugate") !== null)
+	{
+		//TODO when deploying put this statement back in, right now
+		//for testing its breaking things
+		//state = read_cookie("conjugate")
+	}
+	else
+	{
+		bake_cookie("conjugate", state)
+	}
+	setBoxState()
+
+	//TODO Generate a new query (or maybe grab the last one the user used)
+	generateQuery()
+
+	//Setup key events on the button
+	$('#conjugateBox').keydown(function (event) {
+		var keypressed = event.keyCode || event.which;
+		if (keypressed == 13) {
+			checkBtn()
+		}
+	});
+}
+
+
+function checkBtn()
+{
+	//get the current state from the check boxes, save it
+	//to the cookie
+	
+	updateStateFromBoxes();
+
+	//TODO here is where we actually do the checking stuff
+	var thisAttempt = $("#conjugateBox").val();
+
+	//TODO Check if the guessed word is correct
+	if(thisAttempt === state['current']['solution'])
+	{
+		//TODO if the guessed word is correct, generate a new word
+
+	}
+	else
+	{
+		//TODO make this even cooler
+		$("#conjugateBox").effect("shake");
+	}
+
+
+	//Update the state with whatever changed
+	bake_cookie("conjugate", state);
+}
+
+$(document).ready(function(){
+	mainFunc()
+});
---
+++ vocab.js
@@ -0,0 +1,75 @@
+var vocab = 
+[
+	{
+	semester: "fall 2017", 
+	module: "1.1",
+	vocab: [
+		{type: "adjective", plain: "おいしい", english:	"delicious"}, 
+		{type: "noun", plain: "さかな", english:	"fish"}, 
+		{type: "noun", plain: "にく", english:	"meat"}, 
+		{type: "noun", plain: "やさい", english:	"vegetable"}, 
+		{type: "noun", plain: "メニュー", english:	"menu"}, 
+		{type: "noun", plain: "えいが", english:	"movie"}, 
+		{type: "noun", plain: "どくしょ", english:	"reading"}, 
+		{type: "noun", plain: "スポーツ", english:	"sports"}, 
+		{type: "noun", plain: "ダンス", english:	"dance"}, 
+		{type: "noun", plain: "ゲーム", english:	"game"}, 
+		{type: "noun", plain: "アニメ", english:	"anime"}, 
+		{type: "noun", plain: "まんが", english:	"manga"}, 
+		{type: "noun", plain: "ひと", english:	"person"}, 
+		{type: "noun", plain: "しゅくだい", english:	"homework"}, 
+		{type: "noun", plain: "いぬ", english:	"dog"}, 
+		{type: "noun", plain: "ねこ", english:	"cat"}, 
+		{type: "adjective", plain: "たかい", english:	"expensive;high"}, 
+		{type: "phrase", plain: "〜えん", english:	"...yen"}, 
+		{type: "adjective", plain: "やすい", english:	"inexpensive; cheap"}, 
+		{type: "phrase", plain: "〜ドル", english:	"...dollar(s)"}, 
+		{type: "phrase", plain: "いくら", english:	"how much"}
+		]
+	}, 
+	{
+	semester: "fall 2017", 
+	module: "3.1",
+	vocab: [
+		{type: "verb", plain: "あう", english:	"to meet; to see (a person)"},
+		{type: "phrase", plain: "ある", english:	"there is..."},
+		{type: "verb", plain: "かう", english:	"to buy"},
+		{type: "verb", plain: "かく", english:	"to write"},
+		{type: "verb", plain: "いる", english:	"(a person) is in..., stays at..."},
+		{type: "verb", plain: "とる", english:	"to take (a picture)"},
+		{type: "verb", plain: "まつ", english:	"to wait"},
+		{type: "verb", plain: "わかる", english:	"to understand"},
+		{type: "noun", plain: "かいもの", english:	"shopping"},
+		{type: "noun", plain: "アルバイト", english:	"part-time job"},
+		{type: "noun", plain: "いす", english:	"chair"},
+		{type: "noun", plain: "テーブル", english:	"table"},
+		{type: "noun", plain: "つくえ", english:	"desk"},
+		{type: "noun", plain: "まど", english:	"window"},
+		{type: "noun", plain: "ドア", english:	"door"},
+		{type: "noun", plain: "しゃしん", english:	"picture; photograph"},
+		{type: "noun", plain: "メール", english:	"e-mail"},
+		{type: "noun", plain: "てがみ", english:	"letter"},
+		{type: "noun", plain: "おかね", english:	"money"},
+		{type: "noun", plain: "おみやげ", english:	"souvenir"},
+		{type: "noun", plain: "こども", english:	"child"},
+		{type: "noun", plain: "ひと", english:	"person"},
+		{type: "noun", plain: "じかん", english:	"time"},
+		{type: "noun", plain: "バス", english:	"bus"},
+		{type: "adjective", plain: "みぎ", english:	"right"},
+		{type: "adjective", plain: "ひだり", english:	"left"},
+		{type: "adjective", plain: "まえ", english:	"front"},
+		{type: "adjective", plain: "うしろ", english:	"back"},
+		{type: "adjective", plain: "なか", english:	"inside"},
+		{type: "adjective", plain: "そと", english:	"outside"},
+		{type: "adjective", plain: "うえ", english:	"on; above"},
+		{type: "adjective", plain: "した", english:	"under"},
+		{type: "adjective", plain: "ちかく", english:	"near; nearby"},
+		{type: "adjective", plain: "となり", english:	"next (same type)"},
+		{type: "adjective", plain: "よこ", english:	"next"},
+		{type: "adjective", plain: "あいだ", english:	"between"}
+		]
+	}
+]
+
+//console.log(vocab[1]['vocab'])
+
